8ba41e51df01b4f0120741be4b730c20acc6cda7
kanbolotov1ch <118667180+kanbolotov1ch@users.noreply.github.com>
2022-11-20 18:41:08 +0600

    Add files via upload

---
+++ lesson_1/lesson_1.js
@@ -0,0 +1,25 @@
+// оператор присваивания
+let g;
+g = 34;
+console.log(g)
+
+// cоставные операторы присваивания
+let j = 390;
+j += 10;
+console.log(j)
+
+// типы данных
+let fort = "kutman  ";
+fort += "kurbanaliev";
+console.log(fort)
+
+// числовые типы данных
+let number1 = 5;
+number2 = 46;
+console.log(number1, number2);
+
+//условные конструкции
+if (number1 > number2) {
+    console.log("правильно")
+} else {
+    console.log("неправильно")}
